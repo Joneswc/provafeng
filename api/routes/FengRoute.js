274c7122db19e480fe02f49b8777622b6e1703c6
Joneswc <joneswc@gmail.com>
2018-02-23 01:27:46 -0300

    rotas para acessar dados do banco

--- api/routes/FengRoute.js
+++ api/routes/FengRoute.js
@@ -8,4 +8,14 @@ module.exports = function (app) {
   app.route('/tasks/:taskId')
     .get(fengList.read_a_task)
 
+  router.get('/data', (req, res) => {
+    execSQLQuery('SELECT * FROM data', res);
+  })
+
+  router.get('/clientes/:id?', (req, res) => {
+    let filter = '';
+    if (req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM data' + filter, res);
+  })
+
 };
