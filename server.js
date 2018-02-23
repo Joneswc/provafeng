5c6ec760c160e87b57013d4c037f0a594f37cd85
Joneswc <joneswc@gmail.com>
2018-02-23 01:11:25 -0300

    configurações do mysql

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express'),
+var mysql = require('mysql'),
+var connection = mysql.createConnection({
+  connectionLimit: 100,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sql_feng_db'
+}),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
@@ -17,6 +25,35 @@ app.use(function (req, res) {
 
 var routes = require('./api/routes/FengRoutes');
 routes(app);
+
+function handle_database(req, res) {
+
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      connection.release();
+      res.json({ "code": 100, "status": "Error connecting database" });
+      return;
+    }
+
+    console.log('connection id ' + connection.threadId);
+
+    connection.query("select * from data", function (err, rows) {
+      connection.release();
+      if (!err) {
+        res.json(rows);
+      }
+    });
+
+    connection.on('error', function (err) {
+      res.json({ "code": 100, "status": "Error connecting database" });
+      return;
+    });
+  });
+}
+
+app.get("/", function (req, res) {
+  handle_database(req, res);
+});
 
 app.listen(port);
 
