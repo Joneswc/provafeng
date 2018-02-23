336416211548916fb90d48ebf0b401b49158313f
Joneswc <joneswc@gmail.com>
2018-02-23 00:34:53 -0300

    criando controller com os endpoints

---
+++ api/controllers/FengController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Task = mongoose.model('Tasks');
+
+exports.list_all_tasks = function (req, res) {
+    Task.find({}, function (err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+exports.read_a_task = function (req, res) {
+    Task.findById(req.params.taskId, function (err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
